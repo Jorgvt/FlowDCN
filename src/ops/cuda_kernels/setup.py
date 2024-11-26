bd5f0470a8e8e5be40be2ed9d1020f8aa35ef437
帅欧 <shuaiou.ws@taobao.com>
2024-11-26 10:03:36 +0800

    forward cuda

--- src/ops/cuda_kernels/setup.py
+++ src/ops/cuda_kernels/setup.py
@@ -1,41 +1,59 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-# setup(
-#     name='dcn',
-#     ext_modules=[
-#         CUDAExtension('dcn', ['./dcn.cu',],
-#         extra_compile_args={'cxx': [], 'nvcc': [
-#             "-DCUDA_HAS_FP16=1",
-#             "-D__CUDA_NO_HALF_OPERATORS__",
-#             "-D__CUDA_NO_HALF_CONVERSIONS__",
-#             "-D__CUDA_NO_HALF2_OPERATORS__",
-#             "--use_fast_math",
-#             # "-G",
-#             "-O3",
-#         ]}
-#         ),
-#     ],
-#     cmdclass={
-#         'build_ext': BuildExtension
-#     }
-# )
-
-
 setup(
-    name='mycuda',
+    name='dcn_cuda_forward',
     ext_modules=[
-        CUDAExtension('mycuda', ['./backward.cu',],
-                      extra_compile_args={'cxx': [], 'nvcc': [
-                          "-O3",
-                          "-DCUDA_HAS_FP16=1",
-                          "-D__CUDA_NO_HALF_OPERATORS__",
-                          "-D__CUDA_NO_HALF_CONVERSIONS__",
-                          "-D__CUDA_NO_HALF2_OPERATORS__",
-                      ]}
-                      ),
+        CUDAExtension('dcn_cuda_forward', ['./forward.cu',],
+        extra_compile_args={'cxx': [], 'nvcc': [
+            "-DCUDA_HAS_FP16=1",
+            "-D__CUDA_NO_HALF_OPERATORS__",
+            "-D__CUDA_NO_HALF_CONVERSIONS__",
+            "-D__CUDA_NO_HALF2_OPERATORS__",
+            "--use_fast_math",
+            "-O3",
+        ]}
+        ),
     ],
     cmdclass={
         'build_ext': BuildExtension
     }
 )
+
+setup(
+    name='dcn_cuda_backward',
+    ext_modules=[
+        CUDAExtension('dcn_cuda_backward', ['./backward.cu',],
+        extra_compile_args={'cxx': [], 'nvcc': [
+            "-DCUDA_HAS_FP16=1",
+            "-D__CUDA_NO_HALF_OPERATORS__",
+            "-D__CUDA_NO_HALF_CONVERSIONS__",
+            "-D__CUDA_NO_HALF2_OPERATORS__",
+            "--use_fast_math",
+            "-O3",
+        ]}
+        ),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    }
+)
+
+
+# setup(
+#     name='mycuda',
+#     ext_modules=[
+#         CUDAExtension('mycuda', ['./backward.cu',],
+#                       extra_compile_args={'cxx': [], 'nvcc': [
+#                           "-O3",
+#                           "-DCUDA_HAS_FP16=1",
+#                           "-D__CUDA_NO_HALF_OPERATORS__",
+#                           "-D__CUDA_NO_HALF_CONVERSIONS__",
+#                           "-D__CUDA_NO_HALF2_OPERATORS__",
+#                       ]}
+#                       ),
+#     ],
+#     cmdclass={
+#         'build_ext': BuildExtension
+#     }
+# )
